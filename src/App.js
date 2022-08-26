c0629bb93c4eb15428499c7a263ec1ac831d9d77
Diego Novoa <85325546+Yoshiuwu@users.noreply.github.com>
2022-08-26 17:05:30 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
 
 function App() {
+
+  const [data, setData] = useState([])
+
+  const fetchData = async() => {
+    const result = await fetch('https://reqres.in/api/users')
+    const getResult = await result.json()
+    setData(getResult.data)
+    console.log(getResult.data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const sortByAscendingLasName = () => {
+    data.sort()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={() => sortByAscendingLasName}>Ascending</button>
+      {data.map(function(user) {
+        return(<li>{user.first_name} {user.last_name} {user.email}</li>)
+      })}
+    </>
   );
 }
 
